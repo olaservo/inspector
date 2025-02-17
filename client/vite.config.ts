3d216a28c06cafec633bf6d99c5fc06b7d7a94b4
dandeliongold <83384332+dandeliongold@users.noreply.github.com>
2025-02-16 21:23:32 -0700

    Add proxy and ignore config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   },
   resolve: {
     alias: {
