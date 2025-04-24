732c4864198eb66938b11174c9af93eb801197eb
Ola Hungerford <olahungerford@gmail.com>
2025-04-24 07:31:17 -0700

    Set up packages and configs

---
+++ client/playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: 'list',
+  use: {
+    baseURL: process.env.APP_URL || 'http://localhost:6274',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:6274',
+    reuseExistingServer: !process.env.CI,
+  },
+});
