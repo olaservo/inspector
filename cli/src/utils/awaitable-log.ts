442bb6853228d1d87c608898dd6404a3b8343877
cliffhall <cliff@futurescale.com>
2025-11-28 13:49:31 -0500

    Remove all from v2/main

--- cli/src/utils/awaitable-log.ts
+++
@@ -1,7 +0,0 @@
-export function awaitableLog(logValue: string): Promise<void> {
-  return new Promise<void>((resolve) => {
-    process.stdout.write(logValue, () => {
-      resolve();
-    });
-  });
-}
